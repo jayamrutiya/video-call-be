6ecc9706b03a3a1cb4c8621ed049436fe1e5b6fc
jayamrutiya <amrutiyajay98@gmail.com>
2022-12-24 11:16:38 +0530

    init commit

--- prisma/migrations/20221223182011_sh/migration.sql
+++ prisma/migrations/20221223182011_sh/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `email` VARCHAR(191) NOT NULL,
     `name` VARCHAR(191) NULL,
+    `createdAt` TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
 
     UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
